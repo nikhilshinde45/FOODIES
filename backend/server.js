6fed1c0f94e083febf8752267742a0a8d1cb63fb
student nikhildemo <nikhilshinde2257@gmail.com>
2025-09-22 12:47:00 +0530

    connected mongodb

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const cors=require("cors");
+const connectDB=require("./utils/db.js");
+const dotenv=require("dotenv");
+const cookieParser=require("cookie-parser")
+
+dotenv.config();
+const app =express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const corsOptions={
+  origin:`http://localhost:5173`,
+  credentials:true,
+}
+app.use(cors(corsOptions));
+app.get("/",(req,res)=>{
+  return res.status(200).json({
+    message:"I am coming from backend",
+    success:true,
+  });
+});
+const PORT=process.env.PORT||3000;
+
+app.listen(PORT,()=>{
+  connectDB();
+  console.log(`Server is runnig at http://localhost:${PORT}`);
+});
