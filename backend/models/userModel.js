1d964fa8c69b6e1565f224467c35c5dd8aa334af
student nikhildemo <nikhilshinde2257@gmail.com>
2025-09-25 02:09:49 +0530

    userModel updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  password: { type: String, required: true },
   //used for email or account verification
   vfcode: {
     type: String,
