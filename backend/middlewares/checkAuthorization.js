e75c31268d57e0305661d3d5928d08847bc76432
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-04 15:35:01 +0530

    updated checkAuthorization

--- backend/middlewares/checkAuthorization.js
+++ backend/middlewares/checkAuthorization.js
@@ -1,32 +1,33 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const checkAuthorization = (req,res,next)=>{
-  
+const checkAuthorization = (req, res, next) => {
   const token = req.headers.authorization;
- // console.log(token);
-    if(token){
-      try{
-        const decoded = jwt.verify(token.replace('Bearer ',''),process.env.SECRET);
 
-        req.id = decoded.id;
-        req.username = decoded.username;
-        req.privilege = decoded.privilege;
-        req.name = decoded.name;
-        next();
-      }catch(error){
-        res.json({
-          status:401,
-          message:'Unauthorized access denied'
-        });
-      }
-    }else{
+  if (token) {
+    try {
+      const decoded = jwt.verify(
+        token.replace("Bearer ", ""),
+        process.env.SECRET
+      );
+
+      req.id = decoded.id;
+      req.username = decoded.username;
+      req.privilege = decoded.privilege;
+      req.name = decoded.name;
+      next();
+    } catch (error) {
       res.json({
-        status:401,
-        message:'unauthorized access denied'
+        status: 401,
+        message: "Unauthorized access denied",
       });
     }
+  } else {
+    res.json({
+      status: 401,
+      message: "unauthorized access denied",
+    });
+  }
+};
 
-}
-
-module.exports = {checkAuthorization};
+module.exports = { checkAuthorization };
