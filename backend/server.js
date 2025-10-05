887a0e3e3776937262b24cb3260d952dfac1b324
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-05 19:12:07 +0530

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,39 @@
-const express=require("express");
-const cors=require("cors");
-const connecToDB=require("./configs/dbconfig.js");
-const dotenv=require("dotenv");
-const cookieParser=require("cookie-parser");
-const {userRouter}=require('./routes/userRoute.js');
-const {staffRouter} = require('./routes/staffRoutes.js');
+const express = require("express");
+const cors = require("cors");
+const connecToDB = require("./configs/dbconfig.js");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const { userRouter } = require("./routes/userRoute.js");
+const { staffRouter } = require("./routes/staffRoutes.js");
+const { adminRouter } = require("./routes/adminRoutes.js");
+const { customerRouter } = require("./routes/customerRoutes.js");
+const { chefRouter } = require("./routes/chefRoutes.js");
+const { waiterRouter } = require("./routes/waiterRoutes.js");
 
 dotenv.config();
-const app =express();
+const app = express();
 //middlewares
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-const corsOptions={
-  origin:`http://localhost:5173`,
-  credentials:true,
-}
+const corsOptions = {
+  origin: `http://localhost:5173`,
+  credentials: true,
+};
 app.use(cors(corsOptions));
-app.get("/",(req,res)=>{
-  return res.status(200).json({
-    message:"Hey,I am coming from backend",
-    success:true,
-  });
-});
 
-app.use('/user',userRouter);//user authentication routes
-app.use('/staff',staffRouter);//staff authentication routes
 
-const PORT=process.env.PORT||3000;
+app.use("/user", userRouter); //user authentication routes
+app.use("/staff", staffRouter); //staff authentication routes
+app.use("/admin", adminRouter); //router for all admin functions
+app.use("/customer", customerRouter); //router for all user functions
+app.use("/chef", chefRouter); //router for all chef functions
+app.use("/waiter", waiterRouter); //router for all waiter functions
 
-app.listen(PORT,()=>{
- connecToDB();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  connecToDB();
   console.log(`Server is runnig at http://localhost:${PORT}`);
 });
