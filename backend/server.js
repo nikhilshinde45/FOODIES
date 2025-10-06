57645bfc4095371106a0af745b742c3e2d051e7c
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-07 01:49:12 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: `http://localhost:5173`,
+  origin: `https://foodies-hotel.onrender.com`,
   credentials: true,
 };
 app.use(cors(corsOptions));
