91c166c8e9cdf88fa3d6e9eeb94152347615e3f7
student nikhildemo <nikhilshinde2257@gmail.com>
2026-02-06 11:01:08 +0530

    Update CORS with deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin:true,
-  
+  origin: [
+    "https://foodies-4jhp.onrender.com", 
+    "http://localhost:5173"            
+  ],
   credentials: true,
 };
 app.use(cors(corsOptions));
