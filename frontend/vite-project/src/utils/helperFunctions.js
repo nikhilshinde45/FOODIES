2414eda0af481067ce3d5fc3f23863c6484d31ed
student nikhildemo <nikhilshinde2257@gmail.com>
2026-02-09 01:37:46 +0530

    fix baseurl

--- frontend/vite-project/src/utils/helperFunctions.js
+++ frontend/vite-project/src/utils/helperFunctions.js
@@ -145,5 +145,6 @@ export const getCategoryName = (index) =>{
 // function to provide the base URL for API calls
 export function getBaseUrl(){
   return 'https://foodies-backend-vv60.onrender.com'
+  
  
 }
